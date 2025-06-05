0e878bc2140cc1fe92081f403fc94e1def1df36b
ivodesouzabarbosa <ivosouzabarbosa@gmail.com>
2025-06-05 20:38:04 -0300

    Unity 6

--- Assets/Script/EnemyScript/EnemyBaseControl.cs
+++ Assets/Script/EnemyScript/EnemyBaseControl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using static Cinemachine.DocumentationSortingAttribute;
+
 
 public class EnemyBaseControl : MonoBehaviour
 {
